fc057092107a2a0e8aa90dcc009f3119722fb328
Paul Laugesen <paullaugesen@pauls-macbook-pro-2.local>
2018-03-24 19:09:16 -0400

    write state to plist file

--- Get it Done!/TodoListViewController.swift
+++ Get it Done!/TodoListViewController.swift
@@ -119,64 +119,6 @@ class TodoViewController: UITableViewController {
         tableView.reloadData()
     }
 
-//    func getItems() {
-//
-//        if !(dataFilePath?.isFileURL)! {
-//
-//            guard let bundlePath = Bundle.main.path(forResource: "GameData", ofType: "plist") else { return }
-//
-//            do {
-//                try fileManager.copyItem(atPath: bundlePath, toPath: path)
-//            } catch let error as NSError {
-//                print("Unable to copy file. ERROR: \(error.localizedDescription)")
-//            }
-//        }
-//    }
-    
-//    func loadGameData() {
-//        
-//        // getting path to GameData.plist
-//        
-//        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
-//        let documentsDirectory = paths[0] as String
-//        let path = documentsDirectory.stringByAppendingPathComponent("GameData.plist")
-//        
-//        let fileManager = NSFileManager.defaultManager()
-//        
-//        //check if file exists
-//        if(!fileManager.fileExistsAtPath(path)) {
-//            // If it doesn't, copy it from the default file in the Bundle
-//            if let bundlePath = NSBundle.mainBundle().pathForResource("GameData", ofType: "plist") {
-//                
-//                let resultDictionary = NSMutableDictionary(contentsOfFile: bundlePath)
-//                println("Bundle GameData.plist file is --> \(resultDictionary?.description)")
-//                
-//                fileManager.copyItemAtPath(bundlePath, toPath: path, error: nil)
-//                println("copy")
-//            } else {
-//                println("GameData.plist not found. Please, make sure it is part of the bundle.")
-//            }
-//        } else {
-//            println("GameData.plist already exits at path.")
-//            // use this to delete file from documents directory
-//            //fileManager.removeItemAtPath(path, error: nil)
-//        }
-//        
-//        let resultDictionary = NSMutableDictionary(contentsOfFile: path)
-//        println("Loaded GameData.plist file is --> \(resultDictionary?.description)")
-//        
-//        var myDict = NSDictionary(contentsOfFile: path)
-//        
-//        if let dict = myDict {
-//            //loading values
-//            bedroomFloorID = dict.objectForKey(BedroomFloorKey)!
-//            bedroomWallID = dict.objectForKey(BedroomWallKey)!
-//            //...
-//        } else {
-//            println("WARNING: Couldn't create dictionary from GameData.plist! Default values will be used!")
-//        }
-//    }
-
     
     
 }
